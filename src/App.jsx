74c301f42d03009f65a530b40da8f8ce638bc31e
iddrisu latif <iddrisulatif244@gmail.com>
2023-09-29 21:43:29 +0000

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/Sign-in" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
