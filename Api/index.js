121391b0161d629783575ca00b5a5996491ba303
iddrisu latif <iddrisulatif244@gmail.com>
2023-09-30 18:10:32 +0000

    setting up the server

--- Api/index.js
+++ Api/index.js
@@ -5,18 +5,18 @@ dotenv.config();
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is up and running: ", process.env.PORT);
-});
 
 app.get("/", (req, res) => {
   res.send("")
 });
 
 mongoose
-  .connect(process.env.MONGO_DB)
-  .then(() => {
-    console.log("Connected to MongoDB");
+.connect(process.env.MONGO_DB)
+.then(() => {
+  // console.log("Connected to MongoDB");
+  app.listen(process.env.PORT, () => {
+    console.log("Server and database is up and running: ", process.env.PORT);
+  });
   })
   .catch((err) => {
     console.log(err);
