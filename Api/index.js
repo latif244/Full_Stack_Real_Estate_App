99a867cdb64c93794c7dcb0630e924737bf7dc88
iddrisu latif <iddrisulatif244@gmail.com>
2023-10-15 13:06:12 +0000

    creating signup api route

--- Api/index.js
+++ Api/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter  from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 dotenv.config();
+
+
+
 
 const app = express();
 
-
+app.use(express.json())
 
 
 
@@ -25,3 +29,4 @@ mongoose
 
 
   app.use('/Api/user', userRouter);
+  app.use('/Api/auth', authRouter);
