1ed126b36b1aca3071b87e077d422bed93cf6683
iddrisu latif <iddrisulatif244@gmail.com>
2023-10-15 17:04:03 +0000

    creating a middleware and function

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../modules/user.model.js"
 import bcryptjs from "bcryptjs"
 
-export const signup = async (req, res) => {
+
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password: hashedPassword})
@@ -10,7 +11,7 @@ export const signup = async (req, res) => {
         res.status(200).json("User created successfully");
         
     } catch (error) {
-        res.status(500).json(error.message)
+       next(error)
         
     }
 }
--- Api/index.js
+++ Api/index.js
@@ -30,3 +30,15 @@ mongoose
 
   app.use('/Api/user', userRouter);
   app.use('/Api/auth', authRouter);
+
+  app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    })
+
+  }) 
+  
